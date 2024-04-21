33fe471b12c169b433160380461061d4a20dd5ad
Luca Marcelli <luca@marcelli.ch>
2024-04-21 15:00:47 +0200

    stuff

--- proxyworlds.go
+++ proxyworlds.go
@@ -16,6 +16,18 @@ const (
 	REMOTE_TO_LOCAL ConnectionType = 1
 )
 
+var remoteAddresses = map[string]string{
+	"prod.gamev90.portalworldsgame.com":          "44.194.163.69",
+	"ec2-54-92-129-196.compute-1.amazonaws.com":  "54.92.129.196",
+	"ec2-54-198-33-148.compute-1.amazonaws.com":  "54.198.33.148",
+	"ec2-54-162-199-227.compute-1.amazonaws.com": "54.162.199.227",
+	"ec2-3-87-31-158.compute-1.amazonaws.com":    "3.87.31.158",
+	"ec2-3-81-35-200.compute-1.amazonaws.com":    "3.81.35.200",
+	"ec2-54-210-140-11.compute-1.amazonaws.com":  "54.210.140.11",
+}
+
+var remoteAddress = "44.194.163.69" // prod.gamev90.portalworldsgame.com
+
 func handleData(src, dst net.Conn, wg *sync.WaitGroup, connectionType ConnectionType) {
 	defer wg.Done()
 
@@ -35,15 +47,33 @@ func handleData(src, dst net.Conn, wg *sync.WaitGroup, connectionType Connection
 			log.Printf("Relaying packet to process (%d bytes): %x", n, buffer[:n])
 		}
 
+		_, err = dst.Write(buffer[:n])
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		cmd := buffer[:4]
 		raw := bson.Raw(buffer[4:n])
 		if raw.Validate() == nil {
 			log.Printf("Deserialized (%x): %s\n", cmd, raw)
-		}
 
-		_, err = dst.Write(buffer[:n])
-		if err != nil {
-			log.Fatal(err)
+			// Switch server
+			if (cmd[0] == 0x6c || cmd[0] == 0x6d || cmd[0] == 0x6e || cmd[0] == 0x6f) && cmd[1] == 0x00 {
+				remoteHostname := raw.Lookup("m0").Document().Lookup("IP").StringValue()
+				remoteAddress = remoteAddresses[remoteHostname]
+				log.Printf("Changing remote address to %s (%s)", remoteHostname, remoteAddress)
+				break
+			} else if cmd[0] == 0x9b && cmd[1] == 0x00 {
+				remoteHostname := raw.Lookup("m1").Document().Lookup("IP").StringValue()
+				remoteAddress = remoteAddresses[remoteHostname]
+				log.Printf("Changing remote address to %s (%s)", remoteHostname, remoteAddress)
+				break
+			}
+
+			// profile received
+			if cmd[0] == 0x8a && cmd[1] == 0x0e && cmd[2] == 0x00 && cmd[3] == 0x00 {
+				println("profile received")
+			}
 		}
 	}
 }
@@ -51,7 +81,7 @@ func handleData(src, dst net.Conn, wg *sync.WaitGroup, connectionType Connection
 func handleConnection(localSocket net.Conn) {
 	defer localSocket.Close()
 
-	remoteSocket, err := net.Dial("tcp", "44.194.163.69:10001")
+	remoteSocket, err := net.Dial("tcp", remoteAddress+":10001")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,6 +102,15 @@ func main() {
 		log.Fatal(err)
 	}
 	defer localSocket.Close()
+
+	// file, err := os.OpenFile("proxy.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	// if err != nil {
+	// 	log.Fatalf("error opening file: %v", err)
+	// }
+	// defer file.Close()
+
+	// writer := io.MultiWriter(os.Stdout, file)
+	// log.SetOutput(writer)
 
 	for {
 		connection, err := localSocket.Accept()
